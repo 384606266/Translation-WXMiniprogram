2b2fd1bcdc546a87d6c16f0403b518c7d131d887
384606266 <384606266@qq.com>
2022-07-06 20:56:10 +0800

    justify

--- pages/page1/page1.js
+++ pages/page1/page1.js
@@ -1,15 +1,57 @@
+//文字翻译界面
+
 const app = getApp()
 
 Page({
   data: {
     shows: false, //控制下拉列表的显示隐藏，false隐藏、true显示
     shows2: false, //控制第二个下拉列表
-    selectDatas: ['英语', '德语', '法语','西班牙语','简体中文','繁体中文','俄语','日语','韩语','意大利语'], //下拉列表的数据
-    symbols: ['en','de','fr','es','zh','ch_tra','ru','ja','ko','it'], //符号列表
-    indexs: 0, //输入语言的下拉列表下标,
-    indexs2: 0, //目标语言的下拉列表下表
+    indexs: 0, //翻译方向,
+    indexs2: 0, //目标语言
     content: '',
-    result: '翻译结果',
+    result: '',
+    direction : [
+      {value:'1',label:"中文->小语种"},
+      {value:'0',label:"小语种->中文"}
+  ],
+    options: [
+      { value: 'Romanian', label: '罗马尼亚语' },
+      { value: 'Polish', label: '波兰语' },
+      { value: 'Czech', label: '捷克语' },
+      { value: 'Greek', label: '希腊语' },
+      { value: 'Hungary', label: '匈牙利语' },
+      { value: 'Bulgarian', label: '保加利亚语' },
+      { value: 'Latvian', label: '拉脱维亚语' },
+      { value: 'Lithuanian', label: '立陶宛语' },
+      { value: 'Arabic', label: '阿拉伯语' },
+      { value: 'Russian', label: '俄语' },
+      { value: 'Hindi', label: '印地语' },
+      { value: 'Hebrew', label: '希伯来语' },
+      { value: 'Bengalese', label: '孟加拉语' },
+      { value: 'Malaysian', label: '马来西亚语' },
+      { value: 'Farsi', label: '波斯语' },
+      { value: 'Indonesian', label:'印度尼西亚语'},
+      { value: 'Slovenian', label:'斯洛文尼亚语'},
+      { value: 'Serbian', label:'塞尔维亚语'},
+      { value: 'Turkish', label:'土耳其语'},
+      { value: 'Slovak', label:'斯洛伐克语'},
+      { value: 'Vietnamese', label:'越南语'},
+      { value: 'Thai', label:'泰语'},
+      { value: 'Macedonia', label:'马其顿语'},
+      { value: 'Albanian', label:'阿尔巴尼亚语'},
+      { value: 'Estonian', label:'爱沙尼亚语'},
+      { value: 'Bosnian', label:'波斯尼亚语'},
+      { value: 'Azerbaijan', label:'阿塞拜疆语'},
+      { value: 'Byelorussian', label:'白俄罗斯语'},
+      { value: 'Georgian', label:'格鲁尼亚语'},
+      { value: 'Kazakhstan', label:'哈萨克斯坦语'},
+      { value: 'Kampuchean', label:'柬埔寨语'},
+      { value: 'Mongolian', label:'蒙古语'},
+      { value:'Burmese', label:'缅甸语'},
+      { value:'Sinhala', label:'僧伽罗语'},
+      { value:'Tamil', label:'泰米尔语'},
+      { value:'Ukrainian', label:'乌克兰语'}
+    ]
   },
 
   // 点击下拉显示框
@@ -50,31 +92,29 @@ Page({
   },
   uploadString: function (e) {
     var that = this
-    var content = that.data.content;
     // 提交请求
-    console.log(content);
     wx.showLoading();
-    console.log(that.data.symbols[that.data.indexs])
     wx.request({
-      //url: 'https://192.168.1.110:8000/texttranslator/',  //测试用服务器网址
-      url: 'https://10.162.215.80:8000/texttranslator/',
+      url: 'http://202.120.36.7:55000/translate/',
       header:{
-        "content-type": "application/x-www-form-urlencoded"		
+        'content-type': 'application/json'		
       },
       method: "POST",
       data: {		//向服务器发送的信息
-        q: that.data.content,
-        src: that.data.symbols[that.data.indexs],
-        dst: that.data.symbols[that.data.indexs2]
+        uuid: "ABCDEF1234",
+        source: that.data.content,
+        direction: that.data.direction[that.data.indexs],
+        language: that.data.options[that.data.indexs2].value,
+        flag:'0'
       },
       success: (res) => {
-        if (res.data.ok == 1) {
-          this.setData({
-            result: res.data.r	//服务器返回的结果
+        console.log(res.data)
+         this.setData({
+            result: res.data.output	//服务器返回的结果
           })
-        }
-        if (res.ok == 0) {
-          console.log(res.data.error_type)
+        
+        if (res.data.error_info != "") {
+          console.log(res.data.error_info)
         }
       },
       fail: function(res) {
--- pages/page2/page2.js
+++ pages/page2/page2.js
@@ -1,13 +1,55 @@
+const app = getApp()
+
 Page({
   data: {
     shows: false, //控制下拉列表的显示隐藏，false隐藏、true显示
     shows2: false, //控制第二个下拉列表
-    selectDatas: ['英语', '德语', '法语','西班牙语','简体中文','繁体中文','俄语','日语','韩语','意大利语'], //下拉列表的数据
-    symbols: ['en','de','fr','es','zh','ch_tra','ru','ja','ko','it'], //符号列表
     indexs: 0, //输入语言的下拉列表下标,
     indexs2: 0, //目标语言的下拉列表下表
-    tempFilePaths: '/imgs/img5.png',
-    result: '翻译结果' //存储翻译结果
+    tempFilePaths: '/imgs/logo.jpg',
+    result: '',
+    direction : [
+      {value:'1',label:"中文->小语种"},
+      {value:'0',label:"小语种->中文"}
+  ],
+    options: [
+      { value: 'Romanian', label: '罗马尼亚语' },
+      { value: 'Polish', label: '波兰语' },
+      { value: 'Czech', label: '捷克语' },
+      { value: 'Greek', label: '希腊语' },
+      { value: 'Hungary', label: '匈牙利语' },
+      { value: 'Bulgarian', label: '保加利亚语' },
+      { value: 'Latvian', label: '拉脱维亚语' },
+      { value: 'Lithuanian', label: '立陶宛语' },
+      { value: 'Arabic', label: '阿拉伯语' },
+      { value: 'Russian', label: '俄语' },
+      { value: 'Hindi', label: '印地语' },
+      { value: 'Hebrew', label: '希伯来语' },
+      { value: 'Bengalese', label: '孟加拉语' },
+      { value: 'Malaysian', label: '马来西亚语' },
+      { value: 'Farsi', label: '波斯语' },
+      { value: 'Indonesian', label:'印度尼西亚语'},
+      { value: 'Slovenian', label:'斯洛文尼亚语'},
+      { value: 'Serbian', label:'塞尔维亚语'},
+      { value: 'Turkish', label:'土耳其语'},
+      { value: 'Slovak', label:'斯洛伐克语'},
+      { value: 'Vietnamese', label:'越南语'},
+      { value: 'Thai', label:'泰语'},
+      { value: 'Macedonia', label:'马其顿语'},
+      { value: 'Albanian', label:'阿尔巴尼亚语'},
+      { value: 'Estonian', label:'爱沙尼亚语'},
+      { value: 'Bosnian', label:'波斯尼亚语'},
+      { value: 'Azerbaijan', label:'阿塞拜疆语'},
+      { value: 'Byelorussian', label:'白俄罗斯语'},
+      { value: 'Georgian', label:'格鲁尼亚语'},
+      { value: 'Kazakhstan', label:'哈萨克斯坦语'},
+      { value: 'Kampuchean', label:'柬埔寨语'},
+      { value: 'Mongolian', label:'蒙古语'},
+      { value:'Burmese', label:'缅甸语'},
+      { value:'Sinhala', label:'僧伽罗语'},
+      { value:'Tamil', label:'泰米尔语'},
+      { value:'Ukrainian', label:'乌克兰语'}
+    ]
   },
   onLoad: function () {
   },
@@ -29,24 +71,30 @@ Page({
   uploadPhoto: function () {
     var that = this;
     wx.showLoading();
+    console.log(that.data.direction[that.data.indexs].value)
+    console.log(that.data.options[that.data.indexs2].value)
+    console.log(that.data.tempFilePaths[0])
     wx.uploadFile({
       filePath: that.data.tempFilePaths[0],
-      name: 'img',    
-      //url: 'https://192.168.1.110:8000/texttranslator/',  //测试用服务器网址
-      url: 'https://10.162.215.80:8000/imgtranslator/',
+      name: 'image',    
+      url: 'http://202.120.36.7:55000/image/',
+      header: {
+        "Content-Type": "multipart/form-data"
+      },
       formData: {
-        src: that.data.symbols[that.data.indexs],
-        dst: that.data.symbols[that.data.indexs2]
+        uuid: "ABCDEF1234",
+        direction: that.data.direction[that.data.indexs].value,
+        language: that.data.options[that.data.indexs2].value,
+        flag:'0'
       },
       success: (res) => {
-        var data = JSON.parse(res.data)
-        if (data.ok == 1) {
-          this.setData({
-            result: data.r	//服务器返回的结果
-          })
-        }
-        else{
-          console.log(data.error_type)
+        var jsonObj = JSON.parse(res.data);
+        this.setData({
+          result: jsonObj.output	//服务器返回的结果
+        })
+        
+        if (res.data.error_info != "") {
+          console.log(res.data.error_info)
         }
       },
       fail: function(res) {
